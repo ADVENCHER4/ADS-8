bee800048253443a149616a4af0ecf89758ab7bc
ADVENCHER4 <73076392+ADVENCHER4@users.noreply.github.com>
2023-05-04 23:47:28 +0300

    Update train.h

--- include/train.h
+++ include/train.h
@@ -3,7 +3,7 @@
 #define INCLUDE_TRAIN_H_
 
 class Train {
-private:
+ private:
     struct Cage {
         bool light; // состояние лампочки
         Cage* next;
@@ -11,7 +11,8 @@ private:
     };
     int countOp; // счетчик шагов (число переходов из вагона в вагон)
     Cage* first, *last; // точка входа в поезд (первый вагон)
-public:
+    
+ public:
     Train();
     void addCage(bool light); // добавить вагон с начальным состоянием лампочки
     int getLength();          // вычислить длину поезда
